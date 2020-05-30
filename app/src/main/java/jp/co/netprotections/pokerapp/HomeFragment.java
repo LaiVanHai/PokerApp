80298bef4e398295abf3bb2100fa281799a24e45
Lai Van Hai <haibka44@gmail.com>
2020-05-30 21:30:01 +0900

    Add more poker set when click button

--- app/src/main/java/jp/co/netprotections/pokerapp/HomeFragment.java
+++ app/src/main/java/jp/co/netprotections/pokerapp/HomeFragment.java
@@ -1,12 +1,25 @@
 package jp.co.netprotections.pokerapp;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 
 /**
@@ -23,6 +36,16 @@ public class HomeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private Button btnResult;
+    private TextView tvAdd;
+    private EditText edtPoker1;
+    private EditText edtPoker2;
+    private EditText edtPoker3;
+    private EditText edtPoker4;
+    private EditText edtPoker5;
+    private LinearLayout container;
+    private ArrayList<String> listPoker = new ArrayList<String>();
 
     public HomeFragment() {
         // Required empty public constructor
@@ -61,4 +84,100 @@ public class HomeFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnResult = (Button) getView().findViewById(R.id.button_result);
+        tvAdd = (TextView) getView().findViewById(R.id.add_poker);
+        container = (LinearLayout) getView().findViewById(R.id.container);
+        LayoutInflater layoutInflater =
+                (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View subView = layoutInflater.inflate(R.layout.shared_input_poker, null);
+        container.addView(subView);
+        listPoker.add("");
+        CheckInput(subView);
+
+        tvAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LayoutInflater layoutInflater =
+                        (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View view = layoutInflater.inflate(R.layout.shared_input_poker, null);
+                int childCount = container.getChildCount();
+                View beforeView = container.getChildAt(childCount-1);
+                View line = beforeView.findViewById(R.id.line);
+                line.setVisibility(View.VISIBLE);
+                listPoker.add("");
+                container.addView(view);
+                Toast.makeText(getContext(), "追加した", Toast.LENGTH_LONG).show();
+                CheckInput(view);
+            }
+        });
+
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int size = listPoker.size();
+            }
+        });
+    }
+
+    private void CheckInput(View view) {
+        btnResult.setEnabled(false);
+        edtPoker1 = (EditText) view.findViewById(R.id.input_poker_1);
+        edtPoker2 = (EditText) view.findViewById(R.id.input_poker_2);
+        edtPoker3 = (EditText) view.findViewById(R.id.input_poker_3);
+        edtPoker4 = (EditText) view.findViewById(R.id.input_poker_4);
+        edtPoker5 = (EditText) view.findViewById(R.id.input_poker_5);
+        edtPoker1.addTextChangedListener(textWatcher);
+        edtPoker2.addTextChangedListener(textWatcher);
+        edtPoker3.addTextChangedListener(textWatcher);
+        edtPoker4.addTextChangedListener(textWatcher);
+        edtPoker5.addTextChangedListener(textWatcher);
+    }
+
+    private TextWatcher textWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            String strElement;
+            Boolean currentStatus, btnActiveStatus = true;
+
+            int childCount = container.getChildCount();
+            for (int i = 0; i < childCount; i++){
+                View thisChild = container.getChildAt(i);
+                edtPoker1 = (EditText) thisChild.findViewById(R.id.input_poker_1);
+                edtPoker2 = (EditText) thisChild.findViewById(R.id.input_poker_2);
+                edtPoker3 = (EditText) thisChild.findViewById(R.id.input_poker_3);
+                edtPoker4 = (EditText) thisChild.findViewById(R.id.input_poker_4);
+                edtPoker5 = (EditText) thisChild.findViewById(R.id.input_poker_5);
+                String pokerInput1 = edtPoker1.getText().toString().trim();
+                String pokerInput2 = edtPoker2.getText().toString().trim();
+                String pokerInput3 = edtPoker3.getText().toString().trim();
+                String pokerInput4 = edtPoker4.getText().toString().trim();
+                String pokerInput5 = edtPoker5.getText().toString().trim();
+                currentStatus = !pokerInput1.isEmpty() && !pokerInput2.isEmpty() &&
+                    !pokerInput3.isEmpty() && !pokerInput4.isEmpty() && !pokerInput5.isEmpty();
+                if (currentStatus) {
+                    strElement = pokerInput1 + " " + pokerInput2 + " " + pokerInput3 + " " +
+                       pokerInput4 + " " + pokerInput5;
+                    listPoker.set(i, strElement);
+                }
+                btnActiveStatus = currentStatus && btnActiveStatus;
+            }
+            btnResult.setEnabled(btnActiveStatus);
+        }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+
+        }
+    };
+
+
 }
